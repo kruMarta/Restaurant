9570ff819ec8c29c5a833e5e04f3b16aa61e9d45
Oleksandr Panchuk <olexandrpan4uk@gmail.com>
2023-04-29 14:04:23 +0300

    - delivery-menu page added - products.json added

---
+++ js/menu-js/dao.js
@@ -0,0 +1,46 @@
+// Отримуємо елемент, в який будуть записані продукти
+const pizzaList = document.querySelector('.pizza');
+const pastaList = document.querySelector('.pasta');
+const saladList = document.querySelector('.salad');
+const drinkList = document.querySelector('.drink');
+
+// Завантажуємо інформацію з JSON файлу
+fetch('../products.json')
+    .then(response => response.json()) // перетворюємо в JSON об'єкт
+    .then(data => {
+        // отримуємо масив продуктів з JSON
+        const products = data.products;
+
+        // пройтися по кожному продукту та створити HTML код для нього
+        products.forEach(product => {
+            const productHTML = `
+                        <div class="product-container">
+                            <a href="">
+                                <div class="product-img">
+                                    <img src="${product.image}" alt="${product.name}">
+                                </div>
+                            </a>
+                            <div class="row product-desc">
+                                <div class="col-8 text-start"><span class="product-title">${product.name}</span></div>
+                                <div class="col-4 text-end"><span class="product-price">${product.price} ₴</span></div>
+                            </div>
+                            <div class="product-btn text-center">
+                                <span class="product-btn-text">добавити в корзину </span>
+                                <img src="../images/menu/long-arrow.svg" alt="longArrow">
+                            </div>
+                        </div>
+            `;
+
+            // додаємо HTML код до відповідного елементу на сторінці
+            const categoryList = {
+                "Піца": pizzaList,
+                "Паста": pastaList,
+                "Салати": saladList,
+                "Напої": drinkList
+            };
+
+            categoryList[product.category].insertAdjacentHTML('beforeend', productHTML);
+
+        });
+    })
+    .catch(error => console.error(error));
