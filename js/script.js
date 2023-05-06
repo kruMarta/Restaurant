095b8c7354065652ee5cb57cfc131b4213233e17
kruMarta <kruchokmarta@gmail.com>
2023-05-06 19:55:55 +0300

    pc+mobile version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,37 @@
-var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
-var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
-  return new bootstrap.Tooltip(tooltipTriggerEl)
-})
+const navbarCollapse = document.querySelector('.navbar-collapse');
+const overlay = document.querySelector('.overlay');
+const closeBtn = document.querySelector('.close-btn');
 
-function scrollToAnchor() {
-    console.log("scroll");
-    const element = document.getElementById("reserv");
-    element.scrollIntoView({ behavior: "smooth" });
-}
+navbarCollapse.addEventListener('shown.bs.collapse', function() {
+  overlay.style.display = 'block';
+  let translateButton = document.getElementById('ua-img');
+  let currentSrc = translateButton.src;
+  if (currentSrc.includes('/images/ua-button-green.svg')) {
+    translateButton.src = '../images/ua button.svg';
+  } else {
+    translateButton.src = '../images/ua-button-green.svg';
+  }
+});
+
+
+closeBtn.addEventListener('click', function() {
+  navbarCollapse.classList.remove('show');
+  overlay.style.display = 'none';
+  let translateButton = document.getElementById('ua-img');
+  let currentSrc = translateButton.src;
+  if (currentSrc.includes('/images/ua-button-green.svg')) {
+    translateButton.src = '../images/ua button.svg';
+  } else {
+    translateButton.src = '../images/ua-button-green.svg';
+  }
+});
+
+
+const links = document.querySelectorAll('.nav-link');
+
+links.forEach(link => {
+  link.addEventListener('click', () => {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarCollapse.classList.remove('show');
+  });
+});
